e759d578459ab2d778a20a1ae4fbdce1d5fed2a7
SankalpaGunasekara <sankalpa.gunasekara99@gmail.com>
2023-09-06 10:04:00 +0530

    functional changes

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms'// to access input form
 
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
   styleUrls: ['./calc.component.css']
 })
-export class CalcComponent {
+export class CalcComponent implements OnInit{ //NgOnInit() method will be initiated in the begining to 
 
+  constructor(){
+
+  }
+
+  userInput:any;
+  ngOnInit(): void {
+      this.userInput = new FormGroup({
+        text:new FormControl()
+      })
+  }
 }
